9cb8344ff4250b0312943cff5e8b94b7c6977c58
Ayobami <ayobamidele006@gmail.com>
2023-01-05 00:43:27 +0100

    added solution for task 06

---
+++ main.py
@@ -0,0 +1,13 @@
+# print(print.__doc__)
+# Variable declaration
+number = 98
+
+print(f'Programming is like building a multilingual puzzle {number}')
+
+
+# print(type(number))
+# print(name.upper())
+
+# print(int.__doc__)
+
+help(get)
