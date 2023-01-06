4ee69c8035547fd02121fe7e978f96031f791108
Ayobami <ayobamidele006@gmail.com>
2023-01-06 16:31:06 +0100

    added some solutions

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    """ gets element at a particular index 
+    of a list
+
+    Args:
+        my_list (list): list item
+        idx (int): int number
+
+    Returns:
+        item: item
+    """
+    if (0 <= idx < len(my_list)):
+        return my_list[idx]
+    return None
