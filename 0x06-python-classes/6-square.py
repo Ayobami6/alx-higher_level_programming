f7a314bb20d0306bc288df9d68da00c0746f8d5f
Ayobami <ayobamidele006@gmail.com>
2023-01-25 15:29:38 +0100

    fixed bug

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -87,5 +87,5 @@ class Square:
         for i in range(self.__position[1]):
             print("")
         for i in range(self.__size):
-            print(" " * self._position[0], end="")
+            print(" " * self.__position[0], end="")
             print("#" * self.__size)
