20c7adea183cc89718e7b80af6599c628681ba7f
Ayobami <ayobamidele006@gmail.com>
2023-02-07 14:33:06 +0100

    fixed bu

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -19,4 +19,6 @@ class Square(Rectangle):
         Args:
             size (int): Description
         """
+        self.integer_validator("size", size)
         super().__init__(size, size)
+        self.__size = size
--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -19,6 +19,7 @@ class Square(Rectangle):
         Args:
             size (int): Description
         """
+        self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
 
