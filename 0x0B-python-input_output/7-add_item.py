513737fc57c06f8cb2b21e0538078abc2900fc75
Ayobami <ayobamidele006@gmail.com>
2023-02-08 20:29:04 +0100

    add more files

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-""" add item module
+"""add item module
 
-Attributes:
+Deleted Attributes:
     filename (str): Description
 """
 
@@ -10,6 +10,7 @@ import sys
 import os.path
 
 if __name__ == __main__:
+
     save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
     load_from_json_file = __import__(
         '8-load_from_json_file').load_from_json_file
