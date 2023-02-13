5fa643b7f4454ab0bdc2550bf266838ff1354090
Ayobami <ayobamidele006@gmail.com>
2023-02-13 12:27:39 +0100

    added my test folders

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,6 +4,8 @@
 from typing import Optional
 import json
 import csv
+import turtle as t
+# import tkinter as tk
 
 
 class Base:
@@ -105,3 +107,84 @@ class Base:
                 return [cls.create(**item) for item in dict_list]
         except IOError:
             return []
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """ save file to csv
+
+        Args:
+            list_objs (list): List of dicos
+        """
+        filename = cls.__name__ + ".csv"
+        with open(filename, "w", newline="") as file:
+            if list_objs is None or list_objs == []:
+                file.write("[]")
+            else:
+                if cls.__name__ == "Rectangle":
+                    fieldnames = ["id", "width", "height", "x", "y"]
+                else:
+                    fieldnames = ["id", "size", "x", "y"]
+                writer = csv.DictWriter(file, fieldnames=fieldnames)
+                [writer.writerow(obj.to_dictionary()) for obj in list_objs]
+                # for obj in list_objs:
+                #     writer.writerow(obj.to_dictionary())
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """ load from csv file
+
+        Returns:
+            list: List of new object diction instance
+        """
+        filename = cls.__name__ + ".csv"
+        try:
+            with open(filename, "r", newline="") as file:
+                if cls.__name__ == "Rectangle":
+                    fieldnames = ["id", "width", "height", "x", "y"]
+                else:
+                    fieldnames = ["id", "size", "x", "y"]
+                dict_list = csv.DictReader(file, fieldnames=fieldnames)
+                dict_list = [dict([k, int(v)] for k, v in d.items())
+                             for d in dict_list]
+                return [cls.create(**dic) for dic in dict_list]
+        except IOError:
+            return []
+
+    @staticmethod
+    def draw(list_rectangles, list_squares):
+        """ Draws the shapes
+
+        Args:
+            list_rectangles (list): List of rectangle object
+            list_squares (list): List of square object
+        """
+        turt = t.Turtle()
+        turt.screen.bgcolor("black")
+        turt.pensize(4)
+        turt.shape("turtle")
+
+        turt.color("yellow")
+        for rect in list_rectangles:
+            turt.showturtle()
+            turt.up()
+            turt.goto(rect.x, rect.y)
+            turt.down()
+            for i in range(2):
+                turt.forward(rect.width)
+                turt.left(90)
+                turt.forward(rect.height)
+                turt.left(90)
+            turt.hideturtle()
+
+        turt.color("blue")
+        for sq in list_squares:
+            turt.showturtle()
+            turt.up()
+            turt.goto(sq.x, sq.y)
+            turt.down()
+            for i in range(2):
+                turt.forward(sq.width)
+                turt.left(90)
+                turt.forward(sq.height)
+                turt.left(90)
+            turt.hideturtle()
