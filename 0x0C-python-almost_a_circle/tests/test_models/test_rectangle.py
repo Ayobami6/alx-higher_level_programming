d47ccbf10465e1d7b1edbf09a4ff340910d4798d
Ayobami <ayobamidele006@gmail.com>
2023-02-13 12:23:52 +0100

    completed test wave 1

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -2,7 +2,6 @@
 """ Rectangle test module
 """
 
-import os
 import unittest
 from models.rectangle import Rectangle
 from models.base import Base
@@ -30,6 +29,46 @@ class TestRectangle_1st(unittest.TestCase):
 
     def test_rect_from_base(self):
         self.assertIsInstance(Rectangle(4, 5), Base)
+
+    def test_x(self):
+        r2 = Rectangle(4, 5)
+        self.assertEqual(r2.x, 0)
+
+    def test_x_2(self):
+        r2 = Rectangle(4, 5, 5)
+        self.assertEqual(r2.x, 5)
+
+    def test_y(self):
+        r2 = Rectangle(4, 5)
+        self.assertEqual(r2.y, 0)
+
+    def test_y_2(self):
+        r2 = Rectangle(4, 5, 7, 8)
+        self.assertEqual(r2.y, 8)
+
+    def test_all_param(self):
+        r2 = Rectangle(4, 5, 7, 8, 9)
+        self.assertEqual(9, r2.id)
+
+    def test_width_private(self):
+        r2 = Rectangle(4, 5, 7, 8, 9)
+        with self.assertRaises(AttributeError):
+            r2.__width
+
+    def test_height_private(self):
+        r2 = Rectangle(4, 5, 7, 8, 9)
+        with self.assertRaises(AttributeError):
+            r2.__height
+
+    def test_x_private(self):
+        r2 = Rectangle(4, 5, 7, 8, 9)
+        with self.assertRaises(AttributeError):
+            r2.__x
+
+    def test_y_private(self):
+        r2 = Rectangle(4, 5, 7, 8, 9)
+        with self.assertRaises(AttributeError):
+            r2.__y
 
 
 if __name__ == "__main__":
