48124e148c19eaddf4334e72faaf654b1af99ea3
Ayobami <ayobamidele006@gmail.com>
2023-04-16 20:10:13 +0100

    added module docstrings

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""Script that prints the first State object from the database hbtn_0e_6_usa
+    """
 
 from sys import argv
 from model_state import Base, State
@@ -15,7 +17,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
     # Query the first instance of State
-    instance = session.query(State).first()
+    instance = session.query(State).order_by(State.id).first()
     if instance:
         print(instance.id, instance.name, sep=": ")
     else:
