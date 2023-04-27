0fbc08b82b73c0f37229369cab7456bdbd758cb3
Ayobami <ayobamidele006@gmail.com>
2023-04-27 19:58:10 +0100

    change find peak algorith

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,30 +4,25 @@
 
 
 def find_peak(list_of_integers):
-    """ Finds the peak
+    """ Finds a peak in a list of unsorted integers.
 
     Args:
-        list_of_integers (list): list of integers
+        list_of_integers (list): A list of integers.
 
     Returns:
-        int: The peak
+        int or None: The peak integer or None if the list is empty or None.
     """
-    arr_len = len(list_of_integers)
-    mid = arr_len // 2
-    if arr_len < 1 or list_of_integers is None:
+    if not list_of_integers:
         return None
-    if arr_len == 1:
-        return list_of_integers[0]
-    if arr_len == 2:
-        return max(list_of_integers)
 
-    # The peak logic
-    if list_of_integers[mid] >= list_of_integers[mid + 1] and \
-            list_of_integers[mid] >= list_of_integers[mid - 1]:
-        return list_of_integers[mid]
-    if mid >= 0 or list_of_integers[mid] < list_of_integers[mid + 1]:
-        # recurse find_peak for the subset list from mid to the end
-        return find_peak(list_of_integers[mid:])
-    if mid > 0 or list_of_integers[mid] < list_of_integers[mid - 1]:
-        # recurse find_peak for the subset list from the start to the mid
-        return find_peak(list_of_integers[:mid])
+    left, right = 0, len(list_of_integers) - 1
+
+    while left < right:
+        mid = (left + right) // 2
+
+        if list_of_integers[mid] < list_of_integers[mid+1]:
+            left = mid + 1
+        else:
+            right = mid
+
+    return list_of_integers[left]
