46a26ebd7cce518d484428c396bf94c23c89a48a
Ayobami <ayobamidele006@gmail.com>
2023-04-28 18:26:41 +0100

    made file exec and add docstrings to main func

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -6,6 +6,8 @@ import urllib.request
 
 
 def main():
+    """Python script that takes in a URL and an email, sends a POST request
+    """
     url = argv[1]
     response = urllib.request.Request(url)
     with urllib.request.urlopen(response) as response:
