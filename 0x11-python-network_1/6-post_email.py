f8428eac0fa82f55411b82cfec117bbc2acffe39
Ayobami <ayobamidele006@gmail.com>
2023-04-28 18:39:12 +0100

    added documentation

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -6,6 +6,8 @@ import requests
 
 
 def main():
+    """Python script that takes in a URL and an email, sends a POST request
+    """
     url = argv[1]
     response = requests.post(url, data={'email': argv[2]})
     print(response.text)
