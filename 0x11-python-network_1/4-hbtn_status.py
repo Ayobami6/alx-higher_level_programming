05090d71c85b595313e95dc994f778f1f3c3ac3d
Ayobami <ayobamidele006@gmail.com>
2023-04-28 18:20:57 +0100

    added docstrings to main functions

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,6 +6,8 @@ import requests
 
 
 def main():
+    """Python script that fetches https://intranet.hbtn.io/status
+    """
     response = requests.get('https://intranet.hbtn.io/status')
     print("Body response:")
     print("\t- type: {}".format(type(response.text)))
--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -8,6 +8,8 @@ import requests
 
 
 def main():
+    """ Python script that takes in a URL,
+    """
     url = argv[1]
     response = requests.get(url)
     print(response.headers.get('X-Request-Id'))
