443d6b51f3532f975ed3852095a4e144bd82a2fb
Ayobami <ayobamidele006@gmail.com>
2023-05-23 15:16:12 +0100

    added file readme script

---
+++ 0x14-javascript-web_scraping/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: 'airbnb-base',
+    overrides: [],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    rules: {
+        'no-console': 0,
+        'no-restricted-globals': 0,
+        'no-plusplus': 0,
+        'space-before-function-paren': 0,
+    },
+};
